be3ec94e12455aec76aca34f96441630424ed159
AleksandrMalinin <alexander.s.malinin@gmail.com>
2019-05-04 15:19:07 +0300

    Реализует передачу данных через props

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import MainScreen from '../main-screen/main-screen.jsx';
+import PropTypes from 'prop-types';
 
-const App = () => {
-  return <MainScreen/>;
+const App = (movieTitlesArray) => {
+  const {movieTitles} = movieTitlesArray;
+
+  return <MainScreen
+    movieTitles={movieTitles}
+  />;
+};
+
+App.propTypes = {
+  movieTitles: PropTypes.arrayOf(PropTypes.string)
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './components/app/app.jsx';
 
 const init = () => {
+  const movieTitlesArray = [
+    `Fantastic Beasts: The Crimes of Grindelwald`,
+    `Bohemian Rhapsody`,
+    `Macbeth`,
+    `Aviator`
+  ];
+
   ReactDOM.render(
-      <App/>,
+      <App
+        movieTitles={movieTitlesArray}
+      />,
       document.querySelector(`body`)
   );
 };
