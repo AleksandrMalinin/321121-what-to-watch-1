c47d70a3526808df9bdf15391b8ab6480bf56f44
AleksandrMalinin <alexander.s.malinin@gmail.com>
2019-05-28 20:41:24 +0300

    Выносит bind в конструктор

--- src/components/genres-list/genres-list.jsx
+++ src/components/genres-list/genres-list.jsx
@@ -2,12 +2,18 @@ import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 
 class GenresList extends PureComponent {
+  constructor() {
+    super();
+
+    this.onGenreChange = (genre) => this._onGenreChange.bind(this, genre);
+  }
+
   render() {
     const {genres, activeGenre} = this.props;
 
     return <ul className="catalog__genres-list">
       {genres.map((genre, i) => <li className={`catalog__genres-item ` + (activeGenre === genre ? `catalog__genres-item--active` : ``)} key={i}>
-        <a href="#" className="catalog__genres-link" onClick={this._onGenreChange.bind(this, genre)}>{genre}</a>
+        <a href="#" className="catalog__genres-link" onClick={this.onGenreChange(genre)}>{genre}</a>
       </li>)}
     </ul>;
   }
