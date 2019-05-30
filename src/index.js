0011d29b00c3f7ccd64871fc92f68ee04b652c50
AleksandrMalinin <alexander.s.malinin@gmail.com>
2019-05-30 23:27:23 +0300

    Обновляет код инициализации хранилища

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import {Provider} from 'react-redux';
 import {reducer} from './reducer.js';
 import App from './components/app/app.jsx';
 
-const store = createStore(reducer);
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
     <Provider store={store}>
