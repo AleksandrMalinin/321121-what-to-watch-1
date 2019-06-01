6827cb76271cf78b0d00bbd43b14d7a99c301e12
AleksandrMalinin <alexander.s.malinin@gmail.com>
2019-06-01 18:52:01 +0300

    Добавляет тест для хока

--- src/components/genres-list/genres-list.test.js
+++ src/components/genres-list/genres-list.test.js
@@ -6,10 +6,12 @@ import {films} from '../../mocks/films.js';
 const mockGenres = films.map((film) => film.genre);
 
 it(`Filters correctly renders`, () => {
+  const mockFunction = jest.fn();
   const tree = renderer
   .create(<GenresList
     genres={mockGenres}
-    activeGenre={films[0].genre}
+    activeItem={films[0].genre}
+    onChange={mockFunction}
   />)
   .toJSON();
 
---
+++ src/hocs/with-active-item.e2e.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import withActiveItem from './with-active-item.js';
+import {films} from '../mocks/films.js';
+
+// React 16 Enzyme adapter
+Enzyme.configure({adapter: new Adapter()});
+
+const MockComponent = () => <div/>;
+const MockComponentWrapped = withActiveItem(MockComponent);
+
+it(`Should change activeItem when callback is being called`, () => {
+  const wrapper = shallow(<MockComponentWrapped
+    activeItem={films[0].genre}
+  />);
+
+  wrapper.instance().onItemChange(films[0].genre);
+  expect(wrapper.state().activeItem).toEqual(films[0].genre);
+});
