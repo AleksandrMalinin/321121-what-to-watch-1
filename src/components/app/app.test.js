f1ddbdb056981f1f41ff9287353b3b5c0bc9c65b
AleksandrMalinin <alexander.s.malinin@gmail.com>
2019-06-09 19:44:23 +0300

    Добавляет тест на редьюсер

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -3,11 +3,14 @@ import renderer from 'react-test-renderer';
 import {App} from './app.jsx';
 import {films} from '../../mocks/films.js';
 
+const mockGenres = films.map((film) => film.genre);
+
 it(`App correctly renders`, () => {
   const tree = renderer
   .create(<App
     moviesList={films}
     activeGenre={films[0].genre}
+    genres={mockGenres}
   />)
   .toJSON();
 
---
+++ src/mocks/films.js
@@ -0,0 +1,34 @@
+/* eslint camelcase: ["error", {properties: "never"}] */
+
+export const films = [
+  {
+    title: `Fantastic Beasts: The Crimes of Grindelwald`,
+    preview_image: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+    preview_video_link: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+    genre: `Comedies`
+  },
+  {
+    title: `Bohemian Rhapsody`,
+    preview_image: `img/bohemian-rhapsody.jpg`,
+    preview_video_link: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+    genre: `Crime`
+  },
+  {
+    title: `Macbeth`,
+    preview_image: `img/macbeth.jpg`,
+    preview_video_link: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+    genre: `Documentary`
+  },
+  {
+    title: `Aviator`,
+    preview_image: `img/aviator.jpg`,
+    preview_video_link: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+    genre: `Dramas`
+  },
+  {
+    title: `We need to talk about Kevin`,
+    preview_image: `img/we-need-to-talk-about-kevin.jpg`,
+    preview_video_link: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+    genre: `Horror`
+  }
+];
