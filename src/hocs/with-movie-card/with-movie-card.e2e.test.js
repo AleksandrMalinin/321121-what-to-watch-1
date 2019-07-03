690f0cc5b474da92fffdf928f0c487dc0afd02fa
AleksandrMalinin <alexander.s.malinin@gmail.com>
2019-07-03 03:15:37 +0300

    Правит варнинг

--- src/hocs/with-movie-card/with-movie-card.e2e.test.js
+++ src/hocs/with-movie-card/with-movie-card.e2e.test.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import Enzyme, {mount} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import PropTypes from "prop-types";
 import withMovieCard from './with-movie-card.js';
 import {constants} from '../../constants.js';
 
 // React 16 Enzyme adapter
 Enzyme.configure({adapter: new Adapter()});
 
-const MockComponent = (props) => <div handleMouseEnter={props.handleMouseEnter}/>;
-
-MockComponent.propTypes = {
-  handleMouseEnter: PropTypes.func
-};
-
+const MockComponent = () => <div/>;
 const MockComponentWrapped = withMovieCard(MockComponent);
 
 it(`On movie-card mouseenter state changes`, () => {
@@ -23,7 +17,7 @@ it(`On movie-card mouseenter state changes`, () => {
         handleMouseEnter={handleMouseEnter}
       />
   );
-  filmCard.simulate(`mouseEnter`);
+  filmCard.simulate(`mouseenter`);
   setTimeout(() => {
     expect(filmCard.state().isPlaying).toEqual(true);
   }, constants.TIMEOUT);
